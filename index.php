3c7f6c9ad889a240a27fd25ea98551b79bc99cf6
DanielPelaezP <pelaez.1216@gmail.com>
2017-09-30 08:49:51 -0500

    cambios en el index

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
 <!DOCTYPE html>
 <html>
-
-
   <body>
-    <h1>"first php page"</h1>
+    <h1>"MY first php page"</h1>
     <?php
-echo"hello world - Daniel Pelaez";
+    echo"hello world - Daniel Pelaez";
      ?>
-    
+     <h1>Change</h1>
   </body>
 </html>
